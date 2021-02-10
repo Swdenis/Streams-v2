e90fab65f20d8f793ba8ff7c99ee3f98fd24f7cd
Swdenis <swdenis@mail.ru>
2021-02-10 17:03:13 +0100

    Added API support and REST

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import {createStream} from '../../actions';
 
 class StreamCreate extends React.Component {
 
@@ -29,11 +31,12 @@ class StreamCreate extends React.Component {
     )
   }
 
-  onSubmit(formValues) {
-    console.log(formValues)
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues)
   }
 
   render() {
+    //console.log(this.props)
     return(
     <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
       <Field 
@@ -65,7 +68,9 @@ const validate = (formValues) => {
   return errors
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
