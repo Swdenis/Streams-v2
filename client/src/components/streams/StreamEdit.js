0394088b132b915db4fa5ad104ccc8dcd6fd43e0
Swdenis <swdenis@mail.ru>
2021-02-11 17:12:13 +0100

    Added PATCH request

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,10 +1,34 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+import _ from 'lodash';
 
-const StreamEdit = (props) => {
-  console.log(props)
-  return <div>StreamEdit</div>;
-};
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id)
+
+  }
+
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues)
+  }
+
+  render() {
+    if(!this.props.stream) {
+      return <div>Loading...</div>
+    }
+    return(
+    <div>
+    <h3>Edit a Stream</h3>
+    <StreamForm 
+    onSubmit={this.onSubmit} 
+    initialValues={_.pick(this.props.stream, 'title', 'description')}/>
+    </div>
+    )
+  }  
+}
 
 const mapStateToProps = (state, ownProps)=> {
   return({
@@ -12,7 +36,7 @@ const mapStateToProps = (state, ownProps)=> {
   })
 }
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, {fetchStream, editStream})(StreamEdit);
 
 
 
