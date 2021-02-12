9a40a50bcb07d7c22b6e7953194f2c003587e461
Swdenis <swdenis@mail.ru>
2021-02-12 11:28:21 +0100

    Added modal

--- src/actions/index.js
+++ src/actions/index.js
@@ -51,6 +51,7 @@ export const editStream = (id,formValues) => async dispatch => {
 }
 
 export const deleteStream = (id) => async dispatch => {
-    await streams.deleteput(`/streams/${id}`);
+    await streams.delete(`/streams/${id}`);
     dispatch({type: DELETE_STREAM, payload: id })
+    history.push('/');
 }
